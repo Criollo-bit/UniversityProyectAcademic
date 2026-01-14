fdbbef9749d871fc90530d46c1e5536fdf86b017
Maury1994 <119636176+Maury1994@users.noreply.github.com>
2026-01-14 10:16:57 -0500

    version estable

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from '@prisma/config';
 
+// Función simple para decidir la URL
+function getDbUrl() {
+  const args = process.argv.join(' ');
+  
+  // Si es el schema de academico -> Base EducationalDataBase
+  if (args.includes('academico.prisma')) {
+    return "postgresql://postgres:123456@localhost:5432/EducationalDataBase";
+  }
+  
+  // Si es el schema de estudiantes -> Base StudentDataBase
+  if (args.includes('estudiantes.prisma')) {
+    return "postgresql://postgres:123456@localhost:5432/StudentDataBase";
+  }
+
+  // Si es cualquier otro -> Base UniversityDataBase
+  return "postgresql://postgres:123456@localhost:5432/UniversityDataBase";
+}
+
 export default defineConfig({
   datasource: {
-    url: process.env.DATABASE_URL || "postgresql://postgres:090306@localhost:5432/universidad_db?schema=public",
+    url: getDbUrl(),
   },
 });
